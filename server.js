a1808ba0d4f53e14fb16ec033bbc6af7c5008f42
ralphcristofer <rcpfugaban@gmail.com>
2023-11-07 06:06:58 -0500

    Initial creation of note POST request

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ if (process.env.NODE_ENV != 'production') {
 // Import dependencies
 const express = require('express');
 const dbconnection = require('./config/dbconnection');
+const Note = require('./model/note');
 
 // Create express app
 const app = express();
@@ -16,13 +17,17 @@ app.use(express.json());
 // Database connection
 dbconnection();
 
-// Routes
+// Routess
+// Display hello world in the main page
 app.get("/", (req, res) => {
     res.json({ hello: "world" });
 });
 
+// Send data to the app
 app.post("/notes", (req, res) => {
-
+    // Get the sent data
+    const title = req.body.title;
+    const body = req.body.body;
 });
 
 // Starting the server
